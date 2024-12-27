bf165b0a66584406d1994e0ae6dd6a20d8b4a242
moonfeather <moonbite233@gmail.com>
2024-12-27 16:42:11 +0800

    add simple DijstraResolve test

--- tests/benchmark.cpp
+++ tests/benchmark.cpp
@@ -1,20 +1,41 @@
 #include <benchmark/benchmark.h>
 
-static void BM_StringCreation(benchmark::State &state) {
-    for (auto _: state)
-        std::string empty_string;
+#include "NetworkManager.h"
+#include "Router.h"
+
+// // Define another benchmark
+// static void BM_StringCopy(benchmark::State &state) {
+//     std::string x = "hello";
+//     for (auto _: state)
+//         std::string copy(x);
+// }
+//
+// BENCHMARK(BM_StringCopy);
+
+static void BM_RouterDijstraResolve(benchmark::State &state) {
+    const auto router1 = std::make_shared<Router>("1");
+    const auto router2 = std::make_shared<Router>("2");
+    const auto router3 = std::make_shared<Router>("3");
+    const auto router4 = std::make_shared<Router>("4");
+
+    auto nm = NetworkManager::getInstance();
+    nm->stopTimer();
+    nm->addRouter(router1.get());
+    nm->addRouter(router2.get());
+    nm->addRouter(router3.get());
+    nm->addRouter(router4.get());
+
+    nm->connect(router1.get(), router2.get(), 3);
+    nm->connect(router2.get(), router3.get(), 4);
+    nm->connect(router1.get(), router3.get(), 2);
+    nm->connect(router1.get(), router4.get(), 10);
+    nm->connect(router3.get(), router4.get(), 1);
+
+    for (auto _:state) {
+        router1->resolve();
+    }
 }
 
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State &state) {
-    std::string x = "hello";
-    for (auto _: state)
-        std::string copy(x);
-}
-
-BENCHMARK(BM_StringCopy);
+BENCHMARK(BM_RouterDijstraResolve);
 
 BENCHMARK_MAIN();
