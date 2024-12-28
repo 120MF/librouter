3e8479f0e03392f6c0ccc21d699f0065ea9a5378
MoonFeather <moonbite233@gmail.com>
2024-12-28 14:29:08 +0800

    Add Router's doxygen

--- src/Router.cpp
+++ src/Router.cpp
@@ -7,8 +7,8 @@ std::random_device rd_;
 std::mt19937 gen_(rd_());
 std::uniform_int_distribution<> distro_(0, 200);
 
-Router::Router()
-{
+
+Router::Router() {
     uuids::uuid const id = uuids::uuid_system_generator{}();
     this->_uuid = uuids::to_string(id);
     this->_hash = std::hash<std::string>()(_uuid);
@@ -16,8 +16,7 @@ Router::Router()
     _delay = distro_(gen_);
 }
 
-Router::Router(uint16_t delay)
-{
+Router::Router(uint16_t delay) {
     uuids::uuid const id = uuids::uuid_system_generator{}();
     this->_uuid = uuids::to_string(id);
     this->_hash = std::hash<std::string>()(_uuid);
@@ -25,8 +24,7 @@ Router::Router(uint16_t delay)
     _delay = delay;
 }
 
-Router::Router(const std::string& name)
-{
+Router::Router(const std::string &name) {
     uuids::uuid const id = uuids::uuid_system_generator{}();
     this->_uuid = uuids::to_string(id);
     this->_hash = std::hash<std::string>()(_uuid);
@@ -34,8 +32,7 @@ Router::Router(const std::string& name)
     _delay = distro_(gen_);
 }
 
-Router::Router(const std::string& name, uint16_t delay)
-{
+Router::Router(const std::string &name, uint16_t delay) {
     uuids::uuid const id = uuids::uuid_system_generator{}();
     this->_uuid = uuids::to_string(id);
     this->_hash = std::hash<std::string>()(_uuid);
@@ -43,22 +40,22 @@ Router::Router(const std::string& name, uint16_t delay)
     _delay = delay;
 }
 
-void Router::resolve()
-{
+void Router::resolve() {
     const auto nm = NetworkManager::getInstance();
     const auto graph = nm->get_graph();
     _dijkstra_resolver.resolve(graph);
 }
 
-Stack<Router*> Router::getShortestPath(Router* target)
-{
-    have_updated.wait();
-    if (target == this)
-    {
-        Stack<Router*> s;
+Stack<Router *> Router::getShortestPath(Router *target) {
+    if (target == this) {
+        Stack<Router *> s;
         s.push(target);
         return s;
     }
+
+    // Waiting for the resolve task to finish
+    have_updated.wait();
+
     return _dijkstra_resolver.getShortestPath(target);
 }
 
--- src/Router.h
+++ src/Router.h
@@ -13,16 +13,30 @@ class NetworkManager;
 
 class Router {
 public:
+  /// Construct the Router by default.
+  /// Router Delay will be randomly generate, while Router name will be set to " "
   Router();
 
+  /// Construct the Router by giving name. Router Delay will be randomly generate.
+  /// @param name Router's name
   explicit Router(const std::string &name);
 
+  /// Construct the Router by giving delay. Router name will be set to " "
+  /// @param delay Router's delay, mustn't be negative
   explicit Router(uint16_t delay);
 
+  /// Construct the Router by giving name and delay.
+  /// @param name Router's name
+  /// @param delay Router's delay, mustn't be negative
   Router(const std::string &name, uint16_t delay);
 
+  /// Run Dijkstra start from the Router
   void resolve();
 
+  /// Get the shortest path from the router to the target router.
+  /// It will get stuck when the resolve task is running.
+  /// @param target the target router.
+  /// @return a Stack containing the path. use Stack::pop() method to iterate it.
   Stack<Router *> getShortestPath(Router *target);
 
   std::chrono::time_point<std::chrono::system_clock> last_update_timestamp;
@@ -38,11 +52,11 @@ private:
   DijkstraResolver<Router *, uint16_t> _dijkstra_resolver = DijkstraResolver<Router *, uint16_t>(this);
 
 public:
-  [[nodiscard]] const std::string &get_uuid() const {
+  [[nodiscard]] const std::string &uuid() const {
     return _uuid;
   }
 
-  [[nodiscard]] const std::string &get_name() const {
+  [[nodiscard]] const std::string &name() const {
     return _name;
   }
 
