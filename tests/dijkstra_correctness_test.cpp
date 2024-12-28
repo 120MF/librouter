d41219f30f025b99b14c99feaa14f5f37eca6346
MoonFeather <moonbite233@gmail.com>
2024-12-28 12:02:26 +0800

    strange behavior happens

--- tests/dijkstra_correctness_test.cpp
+++ tests/dijkstra_correctness_test.cpp
@@ -3,20 +3,23 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <fstream>
 #include "data_structure/Graph.h"
 #include "algorithm/DijkstraResolver.h"
 
 #undef NDEBUG
 #include <cassert>
 
-typedef boost::adjacency_list<boost::listS, boost::vecS, boost::directedS, boost::no_property, boost::property<
+typedef boost::adjacency_list<boost::listS, boost::vecS, boost::undirectedS, boost::no_property, boost::property<
     boost::edge_weight_t, uint64_t> > BoostGraph;
 typedef boost::graph_traits<BoostGraph>::vertex_descriptor Vertex;
 typedef std::pair<uint64_t, uint64_t> Edge;
 
+std::set<Edge> edge_set;
+
 int main() {
-    constexpr uint64_t num_nodes = 50;
-    constexpr uint64_t num_edges = 400;
+    constexpr uint64_t num_nodes = 20;
+    constexpr uint64_t num_edges = 50;
 
     std::vector<Edge> edge_array;
     std::vector<uint64_t> weights;
@@ -29,9 +32,15 @@ int main() {
         uint64_t u = dis(gen);
         uint64_t v = dis(gen);
         if (u != v) {
-            edge_array.emplace_back(u, v);
-            weights.push_back(weight_dis(gen));
-            ++cnt_edges;
+            Edge edge = std::make_pair(u, v);
+            Edge reverse_edge = std::make_pair(v, u);
+            if (!edge_set.contains(edge) && !edge_set.contains(reverse_edge)) {
+                edge_array.emplace_back(edge);
+                weights.push_back(weight_dis(gen));
+                edge_set.insert(edge);
+                edge_set.insert(reverse_edge);
+                ++cnt_edges;
+            }
         }
     }
 
@@ -60,6 +69,21 @@ int main() {
     std::ranges::reverse(boost_path);
     std::cout << "boost_path size: " << boost_path.size() << std::endl;
     std::cout << "boost_path sum: " << distances[target_vertex] << std::endl;
+
+    // Save edges and weights to a file
+    std::ofstream edge_file("edges_weights.txt");
+    for (size_t i = 0; i < edge_array.size(); ++i) {
+        edge_file << edge_array[i].first << " " << edge_array[i].second << " " << weights[i] << "\n";
+    }
+    edge_file.close();
+
+    // Save shortest path to a file
+    std::ofstream path_file("shortest_path.txt");
+    for (const auto &vertex: boost_path) {
+        path_file << vertex << " ";
+    }
+    path_file << "\n";
+    path_file.close();
 
     Graph<uint64_t, uint64_t> lr_graph;
     for (uint64_t i = 0; i < num_nodes; ++i) {
